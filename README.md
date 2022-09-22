6a8d9e88661d5a2157d4e47b640238720aa2e0de
Pee Tankulrat <{ID}+{username}@users.noreply.github.com>
2022-09-22 14:48:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Welcome to Office Climate Project
+
+This project is a part of experiment with realtime data and experimenting with Astro.
+
+## 🧞 Commands
+
+All commands are run from the root of the project, from a terminal:
+
+| Command                | Action                                             |
+| :--------------------- | :------------------------------------------------- |
+| `npm install`          | Installs dependencies                              |
+| `npm run dev`          | Starts local dev server at `localhost:3000`        |
+| `npm run build`        | Build your production site to `./dist/`            |
+| `npm run preview`      | Preview your build locally, before deploying       |
+| `npm run astro ...`    | Run CLI commands like `astro add`, `astro preview` |
+| `npm run astro --help` | Get help using the Astro CLI                       |
+
+## 👀 Want to learn more?
+
+Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
---
+++ src/components/OfficeSensor.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { computed, reactive } from "vue";
+
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+
+dayjs.extend(localizedFormat);
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByi2BP_2T6fhdY7ujRAp1AQyT2rh92EDk",
+  authDomain: "office-sensors-27e21.firebaseapp.com",
+  projectId: "office-sensors-27e21",
+  storageBucket: "office-sensors-27e21.appspot.com",
+  messagingSenderId: "230838264020",
+  appId: "1:230838264020:web:6f2cabfc8d977bccdfdb41",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const state = reactive({
+  temp: 0,
+  humidity: 0,
+  lastUpdate: dayjs().format("LTS"),
+});
+
+const isCold = computed(() => {
+  return state.temp < 22;
+});
+
+const isHot = computed(() => {
+  return state.temp > 26;
+});
+
+const isComfy = computed(() => {
+  return !isHot && !isCold;
+});
+
+onSnapshot(doc(db, "office-climate-latest", "latest"), (doc) => {
+  const { humidity, temperature, dateTime } = doc.data();
+  state.temp = temperature;
+  state.humidity = humidity;
+  state.lastUpdate = dayjs(dateTime.seconds * 1000).format("LTS");
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <img
+      v-if="isCold"
+      src="https://api.iconify.design/noto/cold-face.svg"
+      width="128"
+      alt="Freezing"
+    />
+    <img
+      v-if="isComfy"
+      src="https://api.iconify.design/tabler/mood-happy.svg"
+      width="128"
+      alt="Comfy"
+    />
+    <img
+      v-if="isHot"
+      src="https://api.iconify.design/fluent-emoji/hot-face.svg"
+      width="128"
+      alt="Hot"
+    />
+    <h1>Temp: {{ state.temp }}</h1>
+    <h1>Humidity: {{ state.humidity }}</h1>
+    <p>As of: {{ state.lastUpdate }}</p>
+  </div>
+</template>
